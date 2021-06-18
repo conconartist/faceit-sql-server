ca581697563918d24ccee92ae96e6e3e91322889
conconartist <conconart.info@gmail.com>
2021-06-17 22:15:10 -0600

    Complete get request with express

---
+++ productsData.js
@@ -0,0 +1,30 @@
+const productsData = [
+    {
+        "id": 1046,
+        "brand": "elate cosmetics",
+        "name": "Celestial Highlight Universal Natural Makeup Creme"
+    },
+    {
+        "id": 1047,
+        "brand": "elate cosmetics",
+        "name": "Intrepid Pressed Natural Eyeshadow Trio"
+    },
+    {
+        "id": 1038,
+        "brand": "elate cosmetics",
+        "name": "Refresh Organic Liquid Foundation"
+    },
+    {
+        "id": 1045,
+        "brand": "axiology",
+        "name": "Axiology Lip to Lid Balmies"
+    },
+    {
+        "id": 1044,
+        "brand": "100% pure",
+        "name": "Fruit Pigmented Ultra Lengthening Mascara"
+    }
+
+
+]
+module.exports = productsData;
--- server.js
+++ server.js
@@ -1,40 +1,45 @@
 const express = require('express');
-const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 const productsData = require('./productsData.js');
 
-app.set('port', process.env.PORT || 3002);
-app.locals.makeup = productsData;
-
-app.get('/api/v2/makeup', (request, response) => {
-    response.status(200).json(app.locals.makeup)
-})
-
 app.use(express.json())
+app.use(bodyParser.json());
 app.use(cors());
 
-app.set('port', 3002)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.locals.products = productsData;
+
+app.get('/', (request, response) => {
+    return response.status(200).json(app.locals.products)
+})
+
+app.set('port', process.env.PORT || 3002);
+
 app.listen(app.get('port'), () => {
     console.log(`Now listening on port ${app.get('port')}!`)
 })
 
-app.post('/api/v2/products', (request, response) => {
-    const id = parseInt(app.locals.makeup.length + 1);
-    const { 
-        brand, 
-        name, 
-        price, 
-        price_sign, 
-        currency, 
-        image_link, 
-        product_link, 
-        website_link, 
-        description, 
-        rating, 
-        category, 
-        product_type, 
-        tag_list
-    } = request.body;
-    app.locals.makeup.push({ id, brand, name, price, price_sign, currency, image_link, product_link, website_link, description, rating, category, product_type, tag_list });
-    response.status(201).json({ id, brand, name, price, price_sign, currency, image_link, product_link, website_link, description, rating, category, product_type, tag_list })
-})
+//eventual table for full data: 
+// app.post('/', (request, response) => {
+//     const id = parseInt(app.locals.makeup.length + 1);
+//     const { 
+//         brand, 
+//         name, 
+//         price, 
+//         price_sign, 
+//         currency, 
+//         image_link, 
+//         product_link, 
+//         website_link, 
+//         description, 
+//         rating, 
+//         category, 
+//         product_type, 
+//         tag_list
+//     } = request.body;
+//     app.locals.makeup.push({ id, brand, name, price, price_sign, currency, image_link, product_link, website_link, description, rating, category, product_type, tag_list });
+//     response.status(201).json({ id, brand, name, price, price_sign, currency, image_link, product_link, website_link, description, rating, category, product_type, tag_list })
+// })
