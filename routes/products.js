f32feaabe60f9369b17db26ddde4f30eded49779
conconartist <conconart.info@gmail.com>
2021-06-20 17:39:55 -0600

    Add post request for mysql table

--- routes/products.js
+++ routes/products.js
@@ -19,6 +19,13 @@ router.post('/', (req, res) => {
     const { brand, name } = req.body;
     if(brand && name) {
       console.log(brand, name)
+      try {
+        db.promise().query(`INSERT INTO PRODUCTS VALUES(NULL, '${brand}', '${name}')`);
+        res.status(201).send({ msg: 'Created Product' });
+      }
+      catch (err) {
+          console.log(err);
+      }
     }
 })
 
